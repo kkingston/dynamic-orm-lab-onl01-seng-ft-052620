002e3ae2710aea7d65549ab34439a99944042d5a
Kayla Kingston <kkingston09@gmail.com>
2020-06-30 12:49:05 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -54,8 +54,7 @@ class InteractiveRecord
   end 
   
   def self.find_by(param)
-    sql ="SELECT * FROM #{self.table_name} WHERE #{param.keys[
-      .to_s} = '#{param.values[0]}'"
+    sql ="SELECT * FROM #{self.table_name} WHERE #{param.keys[0].to_s} = '#{param.values[0]}'"
     DB[:conn].execute(sql)
  end
     
